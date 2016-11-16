057ba031b8ed62c5a5359f07bb2f60f28eb1265c
hutianlai <hutianlai@baidu.com>
2016-11-16 22:13:31 +0800

    add first py.

---
+++ rx.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+#coding:utf8
+
+import os
+import time
+import sys
+import random
+import thread
+import threading
+import urllib
+import httplib
+from threading import current_thread
+
+print "first test"
+
+os.system("pause")
